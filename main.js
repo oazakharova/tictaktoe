7f4639019f1ecec9a28a11b33c3bd8be9bd74967
Olga <o.nikolaichenko@yandex.ru>
2023-10-12 18:12:42 +0300

    hasWon

--- main.js
+++ main.js
@@ -46,9 +46,22 @@ let ticTakToe = {
      * @param {MouseEvent} event 
      */
     cellClickHandler(event) {
+        //выход из функции, если не нужно обрабатывать клик
         if (!this.isCorrectClick(event)) {
             return;
         }
+
+        // заполнение ячейки
+        this.fillCell(event);
+
+        // проверка, произошел ли выигрыш
+        if (this.hasWon()) {
+            this.setStatusStopped();
+            this.sayWonPhase();
+        }
+
+        // изменение фигуры при отсутствии выигрыша
+        this.togglePhase();
     },
 
     /**
@@ -88,8 +101,73 @@ let ticTakToe = {
         let col = +event.target.dataset.col;
 
         return this.mapValues[row][col] === '';
-    }
+    },
 
+    /**
+     * заполнение ячейки, на которую кликнул пользователь в событии
+     * @param {Event} event 
+     */
+    fillCell(event) {
+        // получение координат ячейки, на которую кликнул пользователь
+        let row = +event.target.dataset.row;
+        let col = +event.target.dataset.col;
+
+        // заполнение ячейки и проставление значения в массиве
+        this.mapValues[row][col] = this.phase;
+        event.target.textContent = this.phase;
+    },
+
+    /**
+     * проверка, есть ли выигрышная ситуация на карте
+     * @returns {boolean}
+     */
+    hasWon() {
+        return this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 2, y: 0 }) ||
+            this.isLineWon({ x: 0, y: 1 }, { x: 1, y: 1 }, { x: 2, y: 1 }) ||
+            this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }) ||
+
+            this.isLineWon({ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }) ||
+            this.isLineWon({ x: 1, y: 0 }, { x: 1, y: 1 }, { x: 1, y: 2 }) ||
+            this.isLineWon({ x: 2, y: 0 }, { x: 2, y: 1 }, { x: 2, y: 2 }) ||
+
+            this.isLineWon({ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 2 }) ||
+            this.isLineWon({ x: 0, y: 2 }, { x: 1, y: 1 }, { x: 2, y: 0 })
+    },
+
+    /**
+     * проверкуа выигрышной ситуации на линии
+     * @param {x: int, y: int} a 1-я ячейка
+     * @param {x: int, y: int} b 2-я ячейка
+     * @param {x: int, y: int} c 3-я ячейка
+     * @returns {boolean}
+     */
+    isLineWon(a, b, c) {
+        let value = this.mapValues[a.y][a.x]
+            + this.mapValues[b.y][b.x]
+            + this.mapValues[c.y][c.x];
+        return value === 'XXX' || value === '000';
+    },
+
+    // изменение статуса игры на 'остановлена'
+    setStatusStopped() {
+        this.status = 'stopped';
+    },
+
+    /**
+     * сообщение о победе
+     */
+    sayWonPhase() {
+        let figure = this.phase === 'X'
+            ? 'Крестики'
+            : 'Нолики';
+        alert(`${figure} выиграли !`)
+    },
+
+    togglePhase() {
+        this.phase = this.phase === 'X'
+            ? '0'
+            : 'X';
+    },
 }
 
 // запуск игры после полной загрузки страницы
